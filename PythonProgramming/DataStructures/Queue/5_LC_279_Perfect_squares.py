e323561be7103cf117e57c3681a41ce792c51f47
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-05-26 23:41:05 +0000

    perfect square

---
+++ PythonProgramming/DataStructures/Queue/5_LC_279_Perfect_squares.py
@@ -0,0 +1,15 @@
+'''
+Perfect Squares
+
+Solution
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+'''
+
+'''
+Solution : use DFS to find the minimal number of perfect squares
+
+Given n : it can be made from 1, 4,9.16.... n  assume we select 1
+
+'''
