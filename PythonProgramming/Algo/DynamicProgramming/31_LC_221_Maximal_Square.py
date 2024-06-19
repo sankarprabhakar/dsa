7894b2d07615369dd6393093d18aea05a6f11671
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-06-19 02:39:43 +0000

    maximal square

--- PythonProgramming/Algo/DynamicProgramming/31_LC_221_Maximal_Square.py
+++ PythonProgramming/Algo/DynamicProgramming/31_LC_221_Maximal_Square.py
@@ -26,3 +26,33 @@ n == matrix[i].length
 1 <= m, n <= 300
 matrix[i][j] is '0' or '1'.
 """
+class MaximalSquare(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * n for i in range(m)]
+        self.getadjacentsquares(m,n,0,0, dp,matrix)
+        area = max(max(sublist) for sublist in dp)
+        area = area * area
+        return area
+    def getadjacentsquares(self,m,n,current_row,current_col, dp,matrix):
+        if current_row >= m  or current_col >= n:
+            return 0
+        if dp[current_row][current_col] != 0:
+            return dp[current_row][current_col] 
+        d = self.getadjacentsquares(m,n,current_row+1,current_col, dp,matrix)
+        r = self.getadjacentsquares(m,n,current_row,current_col+1, dp,matrix)
+        l = self.getadjacentsquares(m,n,current_row+1,current_col+1, dp,matrix)
+        if matrix[current_row][current_col] == '1':
+            dp[current_row][current_col]  = 1 + min(d,r,l)
+        return dp[current_row][current_col]
+
+        
+if __name__ == "__main__":
+    ms = MaximalSquare()
+    area = ms.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+    print(area)
