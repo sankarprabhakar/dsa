67f17efd4c5db023b1db5c6c57b769de299da673
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-06-27 17:08:33 +0000

    solution for longest palindrome

--- PythonProgramming/Algo/DynamicProgramming/35_LC_516_longest_palindromic_subseq.py
+++ PythonProgramming/Algo/DynamicProgramming/35_LC_516_longest_palindromic_subseq.py
@@ -4,19 +4,44 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        max_palindrome_subseq = 1
+        max_string_len = 1
         m = len(s)
-        dp = [[0] * m for i in range(m)]
-        for i in range(m):
-            for j in range(i,m):
+        dp = [[1] * m for i in range(m)]
+        diff = 0
+        i = 0
+        j = 0
+        while diff < m:
+            i = 0
+            j = diff
+            diff = diff+1
+            while i < m and j < m:
                 if i == j:
                     dp[i][j] = 1
-                else:
+                elif i+1 == j:
                     if s[i] == s[j]:
-                        dp[i][j] = 1 + dp[i][j-1]
+                        dp[i][j] = 2
                     else:
                         dp[i][j] = dp[i][j-1]
-                    if dp[i][j] > max_palindrome_subseq:
-                        max_palindrome_subseq =  dp[i][j]
-        print(dp)
-        return max_palindrome_subseq
+                else:
+                    if s[i] == s[j]:
+                        dp[i][j] = dp[i+1][j-1] + 2
+                    else:
+                        dp[i][j] = max(dp[i+1][j],dp[i][j-1])
+                if max_string_len < dp[i][j]:
+                    max_string_len = dp[i][j]
+                    largest_i = i
+                    largest_j = j
+                i = i+1
+                j = j+1
+            print(dp)
+        return  max_string_len
+
+
+
+       
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.longestPalindromeSubseq("aabaa")
+    input_str = "aabaa"
+    res = sol.longestPalindromeSubseq(input_str)
+    print(res)
