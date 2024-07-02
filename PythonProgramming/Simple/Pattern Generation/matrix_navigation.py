085025c66dc3fb80643ac730f089802130d9c0c7
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-02 17:21:18 +0000

    matrix

--- PythonProgramming/Simple/Pattern Generation/matrix_navigation.py
+++ PythonProgramming/Simple/Pattern Generation/matrix_navigation.py
@@ -23,3 +23,53 @@ if __name__ == "__main__":
         print("* " * after,end = "\n")
         row +=1
         col-=1
+    # j+ i+ i- j- 
+    # (i_limit =0, j_limit=n-1)
+    # (i_lim)
+
+    print("printing in circular format \n")
+    lr_start = 0
+    lr_end = no_col #-1
+    row_count_top = 0
+
+    tb_start = 1
+    tb_end = no_row # - 1
+    col_count_right = no_row -1
+    rl_start = no_col- 2
+    rl_end = 0
+    row_count_bottom = no_row - 1
+    bt_start = no_row - 2
+    bt_end = 1
+    col_count_left = 0
+    while True:
+        if row_count_top <= row_count_bottom and lr_start <= lr_end:
+            for i in range(lr_start,lr_end):
+                print(matrix[row_count_top][i], end = " ")
+            row_count_top += 1
+            lr_start+=1
+            lr_end-=1
+
+        if col_count_left <= col_count_right and tb_start <= tb_end:
+            for i in range(tb_start,tb_end):
+                print(matrix[i][col_count_right], end=" ")
+            col_count_right -= 1
+            tb_start +=1
+            tb_end -= 1
+        
+        if row_count_bottom >= row_count_top and rl_start >= rl_end:
+            for i in range(rl_end,rl_start+1,-1):
+                print(matrix[i][row_count_bottom], end= " ")
+            row_count_bottom -= 1
+            rl_start-=1
+            rl_end+=1
+        
+        
+        if col_count_right >= col_count_left and bt_start >= bt_end:
+            for i in range(bt_start,bt_end+1,-1):
+                print(matrix[i][col_count_left], end = " ")
+            col_count_left += 1
+            bt_start +=1
+            bt_end -= 1
+
+
+
