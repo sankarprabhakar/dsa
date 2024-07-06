8781d371443235163fb8beef272859a6d61189d8
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-06 15:09:13 +0000

    print diagonal

--- PythonProgramming/Simple/Pattern Generation/matrix_navigation.py
+++ PythonProgramming/Simple/Pattern Generation/matrix_navigation.py
@@ -49,17 +49,45 @@ if __name__ == "__main__":
             left += 1
     
     ##
-    print("\nformat change\n")
+    print("\nPrint Diagonal \n")
     # (0,0) (0,1),(1,0),(2,0),(1,1),(0,2),(0,3),(1,2)
 
     ## number of diagnonals 
     diag = 2*m -1 
-    print(diag)
+    #print(diag)
     import math
     # 4-(4-3+1) 5-5-3+2 
     diag_len = [i if i <=m else (diag+1 - i) for i in range(1,diag+1)]
-    print(diag_len)
-    
+    #print(diag_len)
+    print(matrix)
+    direction = 0
+    for i in range(diag): 
+        if i < m:
+            start_x, start_y = 0,i
+        else:
+            start_x, start_y = i-m+1,m-1
+        """if direction == 1:
+            direction = 0
+        else:
+            direction = 1
+        if direction == 1:
+            start_x, start_y = start_y,start_x
+        print(f" start x , start y = {start_x},{start_y}")
+        """
+
+
+
+
+
+        #print(f"start index = {start_x},{start_y}")
+        
+        print_len = diag_len[i]
+        for i in range(print_len):
+            print(matrix[start_x][start_y], end =" ")
+            start_x+=1
+            start_y-=1
+
+       
     
     #while 
     
