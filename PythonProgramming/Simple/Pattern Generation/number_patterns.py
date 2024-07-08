ddec3aaeb405677ca8a1ced1edf17fd03b8ae64d
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-08 16:04:50 +0000

    commit pattern

---
+++ PythonProgramming/Simple/Pattern Generation/number_patterns.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    n = int(input("Enter level"))
+    for i in range(1,n+1):
+        for j in range(i):
+            print(i, end=" ")
+        print("\n")
+    
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end =" ")
+        print("\n")
