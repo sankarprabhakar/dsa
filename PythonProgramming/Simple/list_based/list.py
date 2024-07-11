68445bea556fac1acdf172c70bad7bbfa4bc585b
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-11 06:00:39 +0000

    list position sorting

--- PythonProgramming/Simple/list_based/list.py
+++ PythonProgramming/Simple/list_based/list.py
@@ -3,6 +3,9 @@ def swapfirstlast():
     data[0],data[-1] = data[-1],data[0]
     print(data)
 
+def swap_two_position(list,pos1, pos2):
+    list[pos1],list[pos2] = list[pos2],list[pos1] 
+    return list
 if __name__ == "__main__":
     swapfirstlast()
-    
+    print(swap_two_position([1,2,3,4,5],0,1))
