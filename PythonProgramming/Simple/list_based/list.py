84d8975e4d6f44a96b767ae1ef1c302b5857903d
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-12 15:34:33 +0000

    len utility

--- PythonProgramming/Simple/list_based/list.py
+++ PythonProgramming/Simple/list_based/list.py
@@ -6,6 +6,11 @@ def swapfirstlast():
 def swap_two_position(list,pos1, pos2):
     list[pos1],list[pos2] = list[pos2],list[pos1] 
     return list
+
+def getlen(list):
+    length = sum(1 for _ in list)
+    return length
 if __name__ == "__main__":
     swapfirstlast()
     print(swap_two_position([1,2,3,4,5],0,1))
+    print(getlen([1,2,3,4,5]))
