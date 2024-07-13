201682e20cd6df67d10c8dcafd1a739b32eed8a2
sankarprabhakar <sankarprabhakar@users.noreply.github.com>
2024-07-13 05:42:24 +0000

    list utility

--- PythonProgramming/Simple/list_based/list.py
+++ PythonProgramming/Simple/list_based/list.py
@@ -10,7 +10,15 @@ def swap_two_position(list,pos1, pos2):
 def getlen(list):
     length = sum(1 for _ in list)
     return length
+
+def checkavailable(list, n):
+    if n in list:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     swapfirstlast()
     print(swap_two_position([1,2,3,4,5],0,1))
     print(getlen([1,2,3,4,5]))
+    print(checkavailable([1,2,3,4,5],5))
